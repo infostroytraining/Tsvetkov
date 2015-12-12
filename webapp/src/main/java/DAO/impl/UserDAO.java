2ef61e80be6071684c0053d5736e8fd3313e4633
Алксндр <@Alex>
2015-12-12 02:04:59 +0200

    UserDAO impl

---
+++ webapp/src/main/java/DAO/impl/UserDAO.java
@@ -0,0 +1,44 @@
+package main.java.DAO.impl;
+
+import java.util.List;
+
+import main.java.DAO.DAO;
+import main.java.entity.User;
+
+public class UserDAO implements DAO<User> {
+
+	public UserDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public User create(User entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User update(User entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
