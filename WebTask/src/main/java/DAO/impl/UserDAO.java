3edd5ffa17fc57fce3813c3a4e2535fbb3897747
Alexander <Alexander@ALEXTS>
2015-12-18 22:03:33 +0200

    Update UserDAO

--- WebTask/src/main/java/DAO/impl/UserDAO.java
+++ WebTask/src/main/java/DAO/impl/UserDAO.java
@@ -4,13 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import main.java.DAO.DAO;
-import main.java.db.BDConnection;
+import main.java.DAO.exception.DAOException;
+import main.java.db.ConnectionHolder;
+import main.java.db.Transaction;
+import main.java.db.TransactionManager;
 import main.java.entity.User;
 
 public class UserDAO implements DAO<User> {
@@ -18,6 +22,8 @@ public class UserDAO implements DAO<User> {
 	private static final String SELECT_USER = "SELECT firstname, lastname, email, login, password, image FROM users.users WHERE id=?;";
 	private static final String UPDATE_USER = "UPDATE users.users SET firstname=?,lastname=?,email=?,login=?,password=?,image=?WHERE id=?;";
 	private static final String GET_USER_BY_USERNAME = "SELECT id FROM users.users WHERE login=?;";
+	private static final String DELETE_BY_ID = "DELETE FROM users.users WEHRE id=?;";
+	private static final String SELECT_ALL_FROM_USERS = "SELECT * FROM users.users;";
 	private static final Logger log = LogManager.getLogger();
 
 	public UserDAO() {
@@ -26,10 +32,12 @@ public class UserDAO implements DAO<User> {
 
 	@Override
 	public User create(User entity) {
+
+		Connection connection = ConnectionHolder.getConnection();
 		log.entry(entity.toString());
 		PreparedStatement statement = null;
 		try {
-			statement = BDConnection.getConnection().prepareStatement(INSERT_USER, statement.RETURN_GENERATED_KEYS);
+			statement = connection.prepareStatement(INSERT_USER, statement.RETURN_GENERATED_KEYS);
 			statement.setString(1, entity.getFirstName());
 			statement.setString(2, entity.getLastName());
 			statement.setString(3, entity.getEmail());
@@ -52,11 +60,12 @@ public class UserDAO implements DAO<User> {
 
 	@Override
 	public User get(int id) {
+		Connection connection = ConnectionHolder.getConnection();
 		User user = new User();
 		log.entry(id);
 		PreparedStatement statement = null;
 		try {
-			statement = BDConnection.getConnection().prepareStatement(SELECT_USER);
+			statement = connection.prepareStatement(SELECT_USER);
 			statement.setInt(1, id);
 			ResultSet rs = statement.executeQuery();
 			if (rs.next()) {
@@ -78,10 +87,11 @@ public class UserDAO implements DAO<User> {
 
 	@Override
 	public User update(User entity) {
+		Connection connection = ConnectionHolder.getConnection();
 		log.entry(entity.toString());
 		PreparedStatement statement = null;
 		try {
-			statement = BDConnection.getConnection().prepareStatement(UPDATE_USER);
+			statement = connection.prepareStatement(UPDATE_USER);
 			statement.setString(1, entity.getFirstName());
 			statement.setString(2, entity.getLastName());
 			statement.setString(3, entity.getEmail());
@@ -101,23 +111,57 @@ public class UserDAO implements DAO<User> {
 
 	@Override
 	public void remove(int id) {
-		// TODO Auto-generated method stub
+		User user = new User();
+		log.entry(id);
+		Connection connection = ConnectionHolder.getConnection();
+		PreparedStatement statement = null;
+		try {
+			statement = connection.prepareStatement(SELECT_USER);
+			statement.setInt(1, id);
+		} catch (SQLException e) {
+			log.error("SQLException during delete USER by ID");
+			e.printStackTrace();
+		}
+		log.exit(user.toString());
 
 	}
 
 	@Override
 	public List<User> getAll() {
-
-		return null;
+		List<User> result = new ArrayList<>();
+		log.info("get all users");
+		Connection connection = ConnectionHolder.getConnection();
+		User user = new User();
+		PreparedStatement statement = null;
+		try {
+			statement = connection.prepareStatement(SELECT_ALL_FROM_USERS);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				user.setFirstName(rs.getString("firstname"));
+				user.setLastName(rs.getString("lastname"));
+				user.setEmail(rs.getString("email"));
+				user.setLogin(rs.getString("login"));
+				user.setPassword(rs.getString("password"));
+				user.setId(rs.getInt("id"));
+				user.setImage(rs.getBytes("image"));
+				result.add(user);
+			}
+		} catch (SQLException e) {
+			log.error("SQLException during read USER by ID");
+			e.printStackTrace();
+		}
+		log.exit(result.size()+ "users in DB");
+		return result;
 	}
 
 	public int getUserIdByUsername(String username) {
+		Connection connection = ConnectionHolder.getConnection();
 		int result = 0;
 		User user = new User();
 		log.entry(username);
 		PreparedStatement statement = null;
 		try {
-			statement = BDConnection.getConnection().prepareStatement(GET_USER_BY_USERNAME);
+			statement = connection.prepareStatement(GET_USER_BY_USERNAME);
 			statement.setString(1, username);
 			ResultSet rs = statement.executeQuery();
 			if (rs.next()) {
@@ -131,4 +175,5 @@ public class UserDAO implements DAO<User> {
 		return result;
 
 	}
+
 }
