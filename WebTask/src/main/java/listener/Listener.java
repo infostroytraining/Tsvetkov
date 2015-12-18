2ff3973b56afe51364fb07ca8d72fef7e652aef8
Alexander <Alexander@ALEXTS>
2015-12-18 22:04:13 +0200

    Listener add userService and captcha

--- WebTask/src/main/java/listener/Listener.java
+++ WebTask/src/main/java/listener/Listener.java
@@ -2,12 +2,13 @@ package main.java.listener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
 import main.java.DAO.impl.UserDAO;
+import main.java.db.TransactionManager;
+import main.java.listener.factory.ServiceFactory;
+import main.java.service.CreateImage;
 import main.java.service.UserService;
 import main.java.validator.Validation;
 
@@ -17,33 +18,38 @@ import main.java.validator.Validation;
  */
 public class Listener implements ServletContextListener {
 
-    private static final String USER_DAO = "USER_DAO";
-   
 	/**
-     * Default constructor. 
-     */
-    public Listener() {
-        // TODO Auto-generated constructor stub
-    }
+	 * Default constructor.
+	 */
+	public Listener() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0)  { 
-         UserDAO userDAO = new UserDAO();
-         ServletContext sc = arg0.getServletContext();
-         sc.setAttribute(USER_DAO, userDAO);
-         Validation validation = new Validation();
-         sc.setAttribute("validation", validation);
-         UserService userService = new UserService(userDAO);
-         sc.setAttribute("userService", userService);
-    }
-	
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ServletContext sc = arg0.getServletContext();
+
+		Validation validation = new Validation();
+		sc.setAttribute("validation", validation);
+		UserService userService = ServiceFactory.getUserService();
+		sc.setAttribute("userService", userService);
+		CreateImage createImage = new CreateImage();
+		sc.setAttribute("createImage", createImage);
+	}
+
 }
